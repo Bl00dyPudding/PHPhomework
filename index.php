eb202c89758262f044c22e8351773cfc70a82935
BloodyPudding <hello@bloodypudding.com>
2019-05-22 04:33:10 +0300

    homework #2

--- index.php
+++ index.php
@@ -11,10 +11,15 @@
 <?php
     $h1 = "<h1 class='h1'>Добро пожаловать на сайт студента GeekBrains!</h1>";
     $title = "<p class='title'>Моя вторая домашняя работа по PHP</p>";
-//  Тут у меня рандомизаторы
+
     $a = mt_rand(-99, 99);
     $b = mt_rand(-99, 99);
     $c = mt_rand(0, 15);
+    $arg1 = mt_rand(-99, 99);
+    $arg2 = mt_rand(-99, 99);
+    $sign = mt_rand(1, 4);
+    $x = mt_rand(-10, 10);
+    $y = mt_rand(-10, 10);
 ?>
 <body>
     <main class="content">
@@ -51,17 +56,17 @@
                 </p>
                 <p><span>A:</span>
                     <?php
-//                  В методичке такого нет, А ЗРЯ! Вот так правильно будет:)
-                    oneMoreTime:
-                    switch ($c) {
-                        case 15:
-                            echo 15;
-                            break;
-                        default:
-                            echo $c . " ";
-                            $c++;
-                            goto oneMoreTime;
-                    }
+//  В методичке такого нет, А ЗРЯ! Вот так правильно будет, я думаю:)
+                        oneMoreTime:
+                        switch ($c) {
+                            case 15:
+                                echo 15;
+                                break;
+                            default:
+                                echo $c . " ";
+                                $c++;
+                                goto oneMoreTime;
+                        }
                     ?>
                 </p>
             </section>
@@ -71,17 +76,105 @@
             </p>
             <p><span>A:</span>
                 <?php
-                    function mathOperations ($x = 1 , $y = 1) {
-                        return "<br>" .
-                               "$x + $y = " . ($x + $y) . "<br>" .
-                               "$x - $y = " . ($x - $y) . "<br>" .
-                               "$x * $y = " . ($x * $y) . "<br>" .
-                               "$x / $y = " . (($y === 0)?'На ноль делить нельзя!':($x / $y));
+                    function mathOperationA ($x = 1 , $y = 1) {
+                        return "$x + $y = " . ($x + $y);
                     }
 
-                    echo mathOperations($a , $b);
+                    function mathOperationS ($x = 1 , $y = 1) {
+                        return "$x - $y = " . ($x - $y);
+                    }
+
+                    function mathOperationM ($x = 1 , $y = 1) {
+                        return "$x * $y = " . ($x * $y);
+                    }
+
+                    function mathOperationD ($x = 1 , $y = 1) {
+                        return "$x / $y = " . (($y === 0)?'На ноль делить нельзя!':($x / $y));
+                    }
+
+                    echo "<br>" . mathOperationA($a , $b);
+                    echo "<br>" . mathOperationS($a , $b);
+                    echo "<br>" . mathOperationM($a , $b);
+                    echo "<br>" . mathOperationD($a , $b);
                 ?>
             </p>
+            </section>
+            <section class="answer">
+                <p><span>Q:</span>  Реализовать функцию с тремя параметрами: function mathOperation($arg1, $arg2, $operation),
+                    где $arg1, $arg2 – значения аргументов, $operation – строка с названием операции. В
+                    зависимости от переданного значения операции выполнить одну из арифметических операций
+                    (использовать функции из пункта 3) и вернуть полученное значение (использовать switch).
+                </p>
+                <p><span>A:</span>
+                    <?php
+                        function mathOperation ($arg1 , $arg2 , $operation) {
+                            switch ($operation) {
+                                case 1:  $x = mathOperationA($arg1, $arg2); break;
+                                case 2:  $x = mathOperationS($arg1, $arg2); break;
+                                case 3:  $x = mathOperationM($arg1, $arg2); break;
+                                case 4:  $x = mathOperationD($arg1, $arg2); break;
+                                default: $x = "Упс!";
+                            }
+                            return $x;
+                        }
+
+                    echo "<br>" . mathOperation($arg1 , $arg2 , $sign);
+                    ?>
+                </p>
+            </section>
+            <section class="answer">
+                <p><span>Q:</span> С помощью рекурсии организовать функцию возведения числа в степень. Формат: function
+                    power($val, $pow), где $val – заданное число, $pow – степень.
+                </p>
+                <p><span>A:</span>
+                    <?php
+                        function power ($val , $pow) {
+                            if ($pow === 0) {
+                                return 1;
+                            }
+                            if ($pow < 0) {
+                                return power (1/$val, -$pow);
+                            }
+                            return $val * power ($val, $pow-1);
+                        }
+
+                        echo "<br> $x в степени $y = " . power ($x , $y);
+                    ?>
+                </p>
+            </section>
+            <section class="answer">
+                <p><span>Q:</span> Написать функцию, которая вычисляет текущее время и возвращает его в формате с
+                    правильными склонениями, например: 22 часа 15 минут, 21 час 43 минуты.
+                </p>
+                <p><span>A:</span>
+                    <?php
+                        function whatTimeIsItNow ($number, $endings) {
+                            $number = $number % 100;
+                            if ($number >= 11 && $number <= 19) {
+                                $ending = $endings[2];
+                            } else {
+                                $i = $number % 10;
+                                switch ($i)
+                                {
+                                    case (1): $ending = $endings[0]; break;
+                                    case (2): $ending = $endings[1]; break;
+                                    case (3): $ending = $endings[1]; break;
+                                    case (4): $ending = $endings[1]; break;
+                                    default:  $ending = $endings[2];
+                                }
+                            }
+                            return $ending;
+                        }
+                        $hours   = date('G');
+                        $minutes = date('m');
+                        $seconds = date('s');
+
+                        echo 'Сейчас ' .
+                            $hours   . ' ' . whatTimeIsItNow($hours,   ['час', 'часа', 'часов']) . ' ' .
+                            $minutes . ' ' . whatTimeIsItNow($minutes, ['минута', 'минуты', 'минут']) . ' ' .
+                            $seconds . ' ' . whatTimeIsItNow($seconds, ['секунда', 'секунды', 'секунд']);
+                    ?>
+                </p>
             </section>
         </div>
         <div class='currentYear'>
@@ -90,4 +183,3 @@
     </main>
 </body>
 </html>
-
