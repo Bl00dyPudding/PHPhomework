2254d9ee2870ccefb505957430a00558c7cd8450
BloodyPudding <hello@bloodypudding.com>
2019-05-23 03:50:59 +0300

    homework #3 (first steps)

--- index.php
+++ index.php
@@ -10,16 +10,7 @@
 </head>
 <?php
     $h1 = "<h1 class='h1'>Добро пожаловать на сайт студента GeekBrains!</h1>";
-    $title = "<p class='title'>Моя вторая домашняя работа по PHP</p>";
-
-    $a = mt_rand(-99, 99);
-    $b = mt_rand(-99, 99);
-    $c = mt_rand(0, 15);
-    $arg1 = mt_rand(-99, 99);
-    $arg2 = mt_rand(-99, 99);
-    $sign = mt_rand(1, 4);
-    $x = mt_rand(-10, 10);
-    $y = mt_rand(-10, 10);
+    $title = "<p class='title'>Моя третья домашняя работа по PHP</p>";
 ?>
 <body>
     <main class="content">
@@ -29,150 +20,44 @@
         <div>
             <?php echo $title;?>
             <section class="answer">
-                <p><span>Q:</span> Объявить две целочисленные переменные $a и $b и задать им произвольные начальные
-                    значения. <br>
-                    Если $a и $b положительные, вывести их разность. <br>
-                    Если $а и $b отрицательные, вывести их произведение. <br>
-                    Если $а и $b разных знаков, вывести их сумму.
+                <p><span>Q:</span> С помощью цикла while вывести все числа в промежутке от 0 до 100, которые делятся на 3 без
+                    остатка.
                 </p>
                 <p><span>A:</span>
                     <?php
-                        echo "<br>A = $a";
-                        echo "<br>B = $b";
+                    $i = 0;
 
-                        if ($a >= 0 && $b >= 0) {
-                            echo "<br>Разность " .  ($a - $b);
-                        } elseif ($a < 0 && $b < 0) {
-                            echo "<br>Произведение " .  ($a * $b);
+                    while ($i <= 100) {
+                        if ($i % 3 === 0) {
+                            echo "$i ";
+                        }
+                        $i++;
+                    }
+                    ?>
+                </p>
+            </section>
+            <section class="answer">
+                <p><span>Q:</span> С помощью цикла do…while написать функцию для вывода чисел от 0 до 10.
+                </p>
+                <p><span>A:</span>
+                    <?php
+                    $n = 10;
+                    $i = 0;
+
+                    function numbers($i) {
+                        if ($i === 0) {
+                            return "$i" . ' - это ноль<br>';
+                        } elseif ($i % 2) {
+                            return "$i" . ' - нечетное число<br>';
                         } else {
-                            echo "<br>Сумма " .  ($a + $b);
+                            return "$i" . ' - четное число<br />';
                         }
-                    ?>
-                </p>
-            </section>
-            <section class="answer">
-                <p><span>Q:</span> Присвоить переменной $c значение в промежутке [0..15]. С помощью оператора
-                    switch организовать вывод чисел от $c до 15.
-                </p>
-                <p><span>A:</span>
-                    <?php
-//  В методичке такого нет, А ЗРЯ! Вот так правильно будет, я думаю:)
-                        oneMoreTime:
-                        switch ($c) {
-                            case 15:
-                                echo 15;
-                                break;
-                            default:
-                                echo $c . " ";
-                                $c++;
-                                goto oneMoreTime;
-                        }
-                    ?>
-                </p>
-            </section>
-            <section class="answer">
-            <p><span>Q:</span> Реализовать основные 4 арифметические операции в виде функций с двумя параметрами.
-                Обязательно использовать оператор return.
-            </p>
-            <p><span>A:</span>
-                <?php
-                    function mathOperationA ($x = 1 , $y = 1) {
-                        return "$x + $y = " . ($x + $y);
                     }
 
-                    function mathOperationS ($x = 1 , $y = 1) {
-                        return "$x - $y = " . ($x - $y);
-                    }
-
-                    function mathOperationM ($x = 1 , $y = 1) {
-                        return "$x * $y = " . ($x * $y);
-                    }
-
-                    function mathOperationD ($x = 1 , $y = 1) {
-                        return "$x / $y = " . (($y === 0)?'На ноль делить нельзя!':($x / $y));
-                    }
-
-                    echo "<br>" . mathOperationA($a , $b);
-                    echo "<br>" . mathOperationS($a , $b);
-                    echo "<br>" . mathOperationM($a , $b);
-                    echo "<br>" . mathOperationD($a , $b);
-                ?>
-            </p>
-            </section>
-            <section class="answer">
-                <p><span>Q:</span>  Реализовать функцию с тремя параметрами: function mathOperation($arg1, $arg2, $operation),
-                    где $arg1, $arg2 – значения аргументов, $operation – строка с названием операции. В
-                    зависимости от переданного значения операции выполнить одну из арифметических операций
-                    (использовать функции из пункта 3) и вернуть полученное значение (использовать switch).
-                </p>
-                <p><span>A:</span>
-                    <?php
-                        function mathOperation ($arg1 , $arg2 , $operation) {
-                            switch ($operation) {
-                                case 1:  $x = mathOperationA($arg1, $arg2); break;
-                                case 2:  $x = mathOperationS($arg1, $arg2); break;
-                                case 3:  $x = mathOperationM($arg1, $arg2); break;
-                                case 4:  $x = mathOperationD($arg1, $arg2); break;
-                                default: $x = "Упс!";
-                            }
-                            return $x;
-                        }
-
-                    echo "<br>" . mathOperation($arg1 , $arg2 , $sign);
-                    ?>
-                </p>
-            </section>
-            <section class="answer">
-                <p><span>Q:</span> С помощью рекурсии организовать функцию возведения числа в степень. Формат: function
-                    power($val, $pow), где $val – заданное число, $pow – степень.
-                </p>
-                <p><span>A:</span>
-                    <?php
-                        function power ($val , $pow) {
-                            if ($pow === 0) {
-                                return 1;
-                            }
-                            if ($pow < 0) {
-                                return power (1/$val, -$pow);
-                            }
-                            return $val * power ($val, $pow-1);
-                        }
-
-                        echo "<br> $x в степени $y = " . power ($x , $y);
-                    ?>
-                </p>
-            </section>
-            <section class="answer">
-                <p><span>Q:</span> Написать функцию, которая вычисляет текущее время и возвращает его в формате с
-                    правильными склонениями, например: 22 часа 15 минут, 21 час 43 минуты.
-                </p>
-                <p><span>A:</span>
-                    <?php
-                        function whatTimeIsItNow ($number, $endings) {
-                            $number = $number % 100;
-                            if ($number >= 11 && $number <= 19) {
-                                $ending = $endings[2];
-                            } else {
-                                $i = $number % 10;
-                                switch ($i)
-                                {
-                                    case (1): $ending = $endings[0]; break;
-                                    case (2): $ending = $endings[1]; break;
-                                    case (3): $ending = $endings[1]; break;
-                                    case (4): $ending = $endings[1]; break;
-                                    default:  $ending = $endings[2];
-                                }
-                            }
-                            return $ending;
-                        }
-                        $hours   = date('G');
-                        $minutes = date('m');
-                        $seconds = date('s');
-
-                        echo 'Сейчас ' .
-                            $hours   . ' ' . whatTimeIsItNow($hours,   ['час', 'часа', 'часов']) . ' ' .
-                            $minutes . ' ' . whatTimeIsItNow($minutes, ['минута', 'минуты', 'минут']) . ' ' .
-                            $seconds . ' ' . whatTimeIsItNow($seconds, ['секунда', 'секунды', 'секунд']);
+                    do {
+                        echo numbers($i);
+                        $i++;
+                    } while ($i <= $n);
                     ?>
                 </p>
             </section>
